0a979b1d329b2af3af64ae6222714d3be963e1e4
alexanderbates <alexander.shakeel.bates@gmail.com>
2016-01-12 21:56:59 +0000

    actually test transformation values for 3 points

--- tests/testthat/test-shootflow.R
+++ tests/testthat/test-shootflow.R
@@ -4,6 +4,11 @@ test_that("we can run shoot and flow on a matrix",{
   m<-read.vtk(vtkfile)
   regdir=system.file("extdata/reg_output", package = 'deformetricar')
   expect_is(deform.m<-shootflow(m, regdir), 'matrix')
+
+  first3_baseline=matrix(c(26.0168, 28.79, 26.0277, 28.3175, 24.1387, 33.2848,
+                           41.1493, 45.1458, 35.4634), nrow=3,
+                         dimnames = list(NULL, c("X", "Y", "Z")))
+  expect_equal(deform.m[1:3,], first3_baseline)
 })
 
 test_that("we can run shoot and flow on a neuonlist",{
