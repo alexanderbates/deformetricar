f41344c9e91371e1b015e4352ee70011bf389633
Gregory Jefferis <jefferis@gmail.com>
2016-01-20 00:20:23 +0000

    tests were _always_ being skipped

--- tests/testthat/test-shootflow.R
+++ tests/testthat/test-shootflow.R
@@ -1,6 +1,7 @@
 context("shoot and flow")
 test_that("we can run shoot and flow on a matrix",{
-  tryCatch(find_app("ShootAndFlow3"), error=skip("Can't find ShootAndFlow3!"))
+  app=try(find_app("ShootAndFlow3"))
+  if(inherits(app, 'try-error')) skip("Can't find ShootAndFlow3!")
   vtkfile=system.file("extdata/vtk_files/larval_central_nervous_system.vtk", package = 'deformetricar')
   m<-read.vtk(vtkfile)
   regdir=system.file("extdata/reg_output", package = 'deformetricar')
@@ -8,7 +9,8 @@ test_that("we can run shoot and flow on a matrix",{
 })
 
 test_that("we can run shoot and flow on a neuronlist",{
-  tryCatch(find_app("ShootAndFlow3"), error=skip("Can't find ShootAndFlow3!"))
+  app=try(find_app("ShootAndFlow3"))
+  if(inherits(app, 'try-error')) skip("Can't find ShootAndFlow3!")
   n3=nat::Cell07PNs[1:3]
   regdir=system.file("extdata/reg_output", package = 'deformetricar')
   expect_is(deform.n3<-shootflow(n3, regdir), 'neuronlist')
