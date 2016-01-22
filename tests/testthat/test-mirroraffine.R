b0aa56c369b622f78e1dc06796bc1cf9c400c380
Gregory Jefferis <jefferis@gmail.com>
2016-01-22 10:36:48 +0000

    Skip test if we can't find shoot and flow

--- tests/testthat/test-mirroraffine.R
+++ tests/testthat/test-mirroraffine.R
@@ -1,5 +1,8 @@
 context("mirroraffine")
 test_that("we can perform a mirroring and affine transformation",{
+  app=try(find_app("ShootAndFlow3"))
+  if(inherits(app, 'try-error')) skip("Can't find ShootAndFlow3!")
+
   n3=nat::Cell07PNs[1:3]
   file=system.file("extdata/fullmirror.rds", package = 'deformetricar')
   expect_is(t.n3<-apply.mirror.affine(n3, pathtomatrix = file), 'neuronlist')
