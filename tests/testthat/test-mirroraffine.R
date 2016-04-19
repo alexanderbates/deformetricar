4e193949e3404ca842c25529750621a22c75c989
alexanderbates <alexander.shakeel.bates@gmail.com>
2016-04-19 17:54:17 -0400

    Removed a test

--- tests/testthat/test-mirroraffine.R
+++ tests/testthat/test-mirroraffine.R
@@ -8,13 +8,3 @@ test_that("we can perform a mirroring and affine transformation",{
   expect_is(t.n3<-apply.mirror.affine(n3, pathtomatrix = file), 'neuronlist')
 })
 
-test_that("transform3d can apply multiple transforms",{
-  m=matrix(101:115, nrow=5, ncol=3)
-  fullmirror=readRDS(system.file("extdata/fullmirror.rds", package = 'deformetricar'))
-  baseline=structure(c(11.5662779431759, 10.702386783413, 9.83849562365012,
-                       8.97460446388725, 8.11071330412435, 98.6329065200601, 99.4694416625517,
-                       100.305976805043, 101.142511947535, 101.979047090026, 103.344312853856,
-                       104.185382590343, 105.026452326829, 105.867522063315, 106.708591799802
-  ), .Dim = c(5L, 3L))
-  expect_equal(transform3dpoints(m, fullmirror), baseline)
-})
